ef488a3e67d1cd0c50ab76a924426f537e0795a6
sunrlse <18810370580@163.com>
2021-04-05 14:45:01 +0800

    check computed no-async & async/await

--- src/pages/admin/user/index.vue
+++ src/pages/admin/user/index.vue
@@ -6,6 +6,8 @@
         Primary
       </a-button> -->
     </div>
+    <div>{{com}}</div>
+    <hr>
     <div>
       <counter />
       <counter />
@@ -37,6 +39,7 @@ export default {
   },
   data() {
     return {
+      co: 1,
       mmp: [
         {
           label: 'all',
@@ -46,6 +49,15 @@ export default {
     }
   },
   computed: {
+    com() {
+      let co = this.co
+      let n = 10
+      setTimeout(() => {
+        n = 100
+      return co + n
+      }, 100);
+      return co + n
+    }
     // mps({mmp}) {
     //   let arr = _.cloneDeep(MP)
     //   return mmp.concat(arr)
@@ -54,6 +66,30 @@ export default {
   created() {
     // 这个_.cloneDeep 方法找不到， 用computed 上面的counter组件也不渲染了，而写在这里，counter就渲染
     this.mps = this.mmp.concat(_.cloneDeep(MP))
+  },
+  mounted() {
+    this.test()
+  },
+  methods: {
+    async getlist() {
+      let list = [12,3]
+      // console.log('async task ')
+      await this.moni() // 这里有await  test方法才会10秒后执行到await后面，去掉await ，test会立即执行完
+      // console.log('async 异步任务结束')
+    },
+    moni() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve([true, true]) 
+        }, 10000);
+      })
+    },
+    async test() {
+      // console.log('test start')
+      let res = this.getlist()
+      // console.log(res)
+      // console.log('test end')
+    }
   }
 }
 </script>
